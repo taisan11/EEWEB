73c88a3d9e88ec5f381a0e22462baeecb2b6dc4e
taisan111111 <tai.kappy55@gmail.com>
2023-10-15 20:15:33 +0900

    もうわかんね～

--- src/EEWEB.ts
+++ src/EEWEB.ts
@@ -2,8 +2,8 @@ import { serve } from 'https://deno.land/std@0.178.0/http/server.ts'
 import { serveFile } from 'https://deno.land/std@0.178.0/http/file_server.ts'
 import * as mod from "https://deno.land/std@0.202.0/path/mod.ts";
 
-function handler(request: Request): Response {
-  return serveFile(request, 'hello.txt')
+async function handler(request: Request): Promise<Response> {
+  return await serveFile(request, 'hello.txt')
 }
 
 serve(handler)
