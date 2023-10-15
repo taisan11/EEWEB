5e2c771f83b14fc6eda43e0b6757bc65f0ca9b16
taisan111111 <tai.kappy55@gmail.com>
2023-10-15 16:34:44 +0900

    🔧 add dnt file

---
+++ dnt.ts
@@ -0,0 +1,47 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.38.1/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+    },
+  package: {
+    name: "EEWEB",
+    version: Deno.args[0],
+    description:
+      "EEWEB",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/taisan11/EEWEB.git",
+    },
+    homepage: "https://github.com/taisan11/EEWEB#readme",
+    license: "MIT",
+    main: "./script/mod.js",
+    types: "./types/mod.d.ts",
+    exports: {
+      ".": {
+        "import": "./esm/mod.js",
+        "require": "./script/mod.js",
+      },
+      "./package.json": "./package.json",
+    },
+    keywords: [
+        "EEWEB"
+    ],
+    engines: {
+      "node": ">=16.0.0",
+    },
+    author: "Ajitomi Daisuke",
+    bugs: {
+      url: "https://github.com/taisan11/EEWEB/issues",
+    },
+  },
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
